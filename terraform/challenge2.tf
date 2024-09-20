fa605ecadd3f16333ced60dde3a37c46c607813e
marions <marions@spotify.com>
2024-09-20 11:03:51 +0200

    challenge3

--- terraform/challenge2.tf
+++ terraform/challenge2.tf
@@ -9,5 +9,5 @@ resource "google_storage_bucket" "bucket-challenge2" {
 resource "google_storage_bucket_iam_member" "bucket-iam-challenge2" {
   bucket = google_storage_bucket.bucket-challenge2.name
   role   = "roles/storage.objectViewer"
-  member = format("serviceAccount:%s", google_service_account.leaked-account-challenge-1.account_id)
+  member = format("serviceAccount:%s", google_service_account.leaked-account-challenge-1.email)
 }
