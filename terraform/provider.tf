6f80a5c4ef713fff9a895ef80e85668f2fb516ac
Marcus Hallberg <mhallberg@spotify.com>
2024-09-24 09:37:38 +0200

    Updated terraform config with GCS

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,3 +10,11 @@ terraform {
 provider "google" {
   project = var.project-id
 }
+
+# GCS bucket created here: https://console.cloud.google.com/storage/browser/crowdstrike-rtbt-testing-terraform/terraform?project=hedonari-ablegu-oused-mit
+terraform {
+  backend "gcs" {
+    bucket  = "bsidesnyc2024terraform"
+    prefix  = "terraform/challenges/state"
+  }
+}
