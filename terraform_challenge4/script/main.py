ad417abbfadd5fbe5edc08d7b3f09866b5dcf689
marion <marion@gitlab.intern.n0jam.de>
2024-09-30 22:55:51 +0200

    function allows metadata path as input

--- terraform_challenge4/script/main.py
+++ terraform_challenge4/script/main.py
@@ -1,39 +1,28 @@
-#import functions_framework
+from datetime import datetime
 import json
 import requests
 
 
 #@functions_framework.http
-def hello_http(request):
+def compute_engine_monitoring(request):
     """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    Heartbeat function monitoring compute engines
+    Dev version - Work in progress
     """
     request_json = request.get_json(silent=True)
-    print(request_json)
 
-    response_dict = {"flag4": "You found flag 4!", "response": ""}
+    response_dict = {}
 
 
-    if request_json and 'url' in request_json:
-        url = request_json['url']
-    else:
-        print("no url found")
-        response_dict["response"] = "invalid input"
-        return json.dumps(response_dict)
+    if request_json and 'metadata' in request_json:
+        metadata = request_json['metadata']
+        # for IAM debugging purposes to check the functions service account - security recommended to remove this afterwards
+        metadata_response = requests.get(f"http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/{metadata}", headers={"Metadata-Flavor": "Google"})
+        response_dict["function_account"] = metadata_response.text
+        if metadata == "token":
+            response_dict["flag4"] = "You found flag 4!"
 
-#    metadata = requests.get("http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token", headers={"Metadata-Flavor": "Google"})
-# 
-    if (url.startswith("http://") or url.startswith("https://")):
-        response = requests.get(url, headers={"Metadata-Flavor": "Google"})
-        response_dict["response"] = response.text
-        return json.dumps(response_dict)
-
-    response_dict["response"] = "invalid input"
+    # compute engine monitoring
+    response_dict["compute_engine_heartbeat"] = f"compute engine was running at {datetime.today().strftime('%Y-%m-%d %H:%M:%S')}"
+ 
     return json.dumps(response_dict)
-
