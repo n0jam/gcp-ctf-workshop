1358b2253df63de9900a90a0deb70cf139b3b9f4
marions <marions@spotify.com>
2024-10-01 08:54:03 +0200

    add function invocation script

--- terraform_challenge4/script/main.py
+++ terraform_challenge4/script/main.py
@@ -3,7 +3,6 @@ import json
 import requests
 
 
-#@functions_framework.http
 def compute_engine_monitoring(request):
     """HTTP Cloud Function.
     Heartbeat function monitoring compute engines
@@ -13,16 +12,20 @@ def compute_engine_monitoring(request):
 
     response_dict = {}
 
-
-    if request_json and 'metadata' in request_json:
-        metadata = request_json['metadata']
+    if request_json and "metadata" in request_json:
+        metadata = request_json["metadata"]
         # for IAM debugging purposes to check the functions service account - security recommended to remove this afterwards
-        metadata_response = requests.get(f"http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/{metadata}", headers={"Metadata-Flavor": "Google"})
+        metadata_response = requests.get(
+            f"http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/{metadata}",
+            headers={"Metadata-Flavor": "Google"},
+        )
         response_dict["function_account"] = metadata_response.text
         if metadata == "token":
             response_dict["flag4"] = "You found flag 4!"
 
     # compute engine monitoring
-    response_dict["compute_engine_heartbeat"] = f"compute engine was running at {datetime.today().strftime('%Y-%m-%d %H:%M:%S')}"
- 
+    response_dict["compute_engine_heartbeat"] = (
+        f"compute engine was running at {datetime.today().strftime('%Y-%m-%d %H:%M:%S')}"
+    )
+
     return json.dumps(response_dict)
